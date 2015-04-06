66cf0f619ffcb87f6bdf231f59cba581b0433db9
David <david.abrahams@students.olin.edu>
2015-04-06 15:13:21 -0400

    cleaned up code

--- api/locu_setup.py
+++ api/locu_setup.py
@@ -1,99 +1,113 @@
-from locu import MenuItemApiClient
+import urllib
 import urllib2
-from urllib import urlencode
-import pprint
 import json
 
-from httplib2 import Http
+LOCU_BASE = 'http://api.locu.com/v1_0/venue/search/?'
+API_KEY = 'f60f052cf5d0473b25022a62a73b107cf0db0aad'
 
-def get_ID(name,locality):
-    name = name.replace(" ","%20")
-    name = name.replace("'","%27")
 
-    url = 'http://api.locu.com/v1_0/venue/search/?name='+name+'&locality='+locality+'&api_key=f60f052cf5d0473b25022a62a73b107cf0db0aad'
+def get_locu_url(name, locality):
+    """
+    :param name: The name of a restaurant
+    :param locality: The town/city a restaurant is in (check locality documentation at https://dev.locu.com/documentation/v1/)
+    :return: A url to query the locu api
+    """
+    return LOCU_BASE + urllib.urlencode([('name', name), ('locality', locality), ('api_key', API_KEY)])
+
+
+def get_ID(name, locality):
+    """
+    :param name: The name of a restaurant
+    :param locality: The town/city a restaurant is in (check locality documentation at https://dev.locu.com/documentation/v1/)
+    :return: The business ID of the restaurant, or None if that restaurant is not found in the locality
+    """
+
+    url = get_locu_url(name, locality)
 
     f = urllib2.urlopen(url)
 
-    try:
-        response_text = f.read()
-        response_data = json.loads(response_text)
+    response_text = f.read()
+    response_data = json.loads(response_text)
+    objects = response_data["objects"]
 
-
+    if len(objects) == 0:
+        return None
+    else:
         return response_data["objects"][0]["resource_uri"]
 
-    except:
-        return "We're sorry! Price data isn't available. Check the website!"
 
-def get_menu(name,locality):
-    ID = get_ID(name,locality)
-    #print ID
+def get_menu(name, locality):
+    """
 
-    url = 'http://api.locu.com'+ID+'?api_key=f60f052cf5d0473b25022a62a73b107cf0db0aad'
-    #print url
-    try: 
+    :param name: The name of a restaurant
+    :param locality: The town/city a restaurant is in (check locality documentation at https://dev.locu.com/documentation/v1/):
+    :return: menu, a list of dicts
+    """
+    ID = get_ID(name, locality)
+    if ID != None:
+        url = 'http://api.locu.com' + ID + '?' + urllib.urlencode([('api_key', API_KEY)])
+
         f = urllib2.urlopen(url)
         response_text = f.read()
         response_data = json.loads(response_text)
-
-        return response_data
-
-    except:
-        return "We're sorry! Price data isn't available. Check the website!"
+        return response_data["objects"][0]["menus"]
+    else:
+        return None
 
 
-def filter_data(name,locality):
-    try:
-        response_data = get_menu(name,locality)["objects"][0]["menus"]
-        
-    except:
-        return "We're sorry! Price data isn't available. Check the website!"
+def filter_data(name, locality):
+    response_data = get_menu(name, locality)
 
-    prices = []  
+    if response_data == None:
+        return None
+
+    prices = []
     key = 'price'
 
     for entry in response_data:
-        sections = entry['sections']
-        
-        for i in range(len(sections)):
-            temp = sections[i]
-            dict1 = temp['subsections']
-            
-            for j in range(len(dict1)):
-                temp1 = dict1[j]
-                dict2 = temp1['contents']
 
-                for k in range(len(dict2)):
-                    temp2 = dict2[k]
-                   
-                    try:
-                        money = (temp2['price'].encode('utf-8'))
+        sections = entry['sections']
+
+        for section in sections:
+
+            subsections = section['subsections']
+
+            for subsection in subsections:
+
+                contents = subsection['contents']
+
+                for content in contents:
+
+                    if 'price' in content:
+
+                        money = (content['price'].encode('utf-8'))
 
                         try:
                             money = float(money)
                             prices.append(money)
-                            
-                        except:
+                        except ValueError:
+                            #If the string can't be converted to a float, leave it the fuck alone
                             pass
-
-                    except:
-                        pass
 
     return prices
 
-def get_topthirty(name,locality):
-    
-    prices = filter_data(name,locality)
-    
-    if prices != "We're sorry! Price data isn't available. Check the website!":
-        sorted_prices = sorted(prices,reverse=True)
-        
-        num = int(.25*len(sorted_prices))
-        num1 = int(.5*len(sorted_prices))
-        return int(sum(sorted_prices[num:num1])/(num1-num))
-    
+
+def get_topthirty(name, locality):
+    prices = filter_data(name, locality)
+
+    if prices != None:
+
+        sorted_prices = sorted(prices, reverse=True)
+
+        num = int(round(.25 * len(sorted_prices)))
+        num1 = int(round(.5 * len(sorted_prices)))
+        return round(sum(sorted_prices[num:num1]) / (num1 - num), 2)
     else:
-        return "We're sorry! Price data isn't available. Check the website!"
+        return None
+
 
 if __name__ == '__main__':
-    get_topthirty(name,locality)
+    name = 'Neptune Oyster'
+    locality = 'Boston'
+    print get_topthirty(name, locality)
 
